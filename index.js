44a7425848a06daf76decda3814da76e8197738c
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-01-02 14:30:31 -0500

    Adds basic html linking and string replacement

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const http = require('http');
 const fs = require('fs');
 
+const madlib = fs.readFileSync('./madlib.html', 'utf8');
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {
         'Content-Type':'text/html'
     })
 
-    res.end('<h1>stuff</h1>');
+    const reqUrl = req.url.slice(1);
+    let content = madlib.replace("***NAME***", reqUrl);
+    res.end(content);
 });
 
 server.listen(3001, () => {
-    console.log("Listening on port 3000")
+    console.log("Listening on port 3001")
 });
